1c6adcc9f1457bb452c49e286eec9c118c93af89
amrit singh bains <amrit.singh.bains@live.com>
2016-01-11 14:11:50 -0500

    added back end, TODO new repo

--- Backend Server/node.js
+++ Backend Server/node.js
@@ -32,23 +32,10 @@ var inputFile='list.csv';
 
 var parser = parse({delimiter: ','}, function (err, data) {
   async.eachSeries(data, function (line, callback) {
-      // when processing finishes invoke the callback to move to the next one
-
-      // records.push({
-      // 	ucDirectory_UcListing_lblOwner: line[117] + " " + line[119],
-      //   ucDirectory_UcListing_lblTitle: line[119],
-      //   ucDirectory_UcListing_lblAddress1: line[35],
-      //   ucDirectory_UcListing_lblCity: line[39],
-      //   ucDirectory_UcListing_lblStateProvince: line[40],
-      //   ucDirectory_UcListing_lblZipPostal: line[38],
-      //   ucDirectory_UcListing_lblFax: line[25],
-      //   ucDirectory_UcListing_lblPhone1: line[23],
-      //   ucDirectory_UcListing_hlEmail: line[121],
-      //   ucDirectory_UcListing_hlWebsite: line[30]
-      // });
-
+    // when processing finishes invoke the callback to move to the next one
   	records.push({
-      	ucdirectory_uclisting_lblowner: line[117] + " " + line[119],
+        ucdirectory_uclisting_lblcompanyname: line[1],
+      	ucdirectory_uclisting_lblowner: line[115] + " " + line[117],
         ucdirectory_uclisting_lbltitle: line[119],
         ucdirectory_uclisting_lbladdress1: line[35],
         ucdirectory_uclisting_lblcity: line[39],
@@ -59,48 +46,45 @@ var parser = parse({delimiter: ','}, function (err, data) {
         ucdirectory_uclisting_hlemail: line[121],
         ucdirectory_uclisting_hlwebsite: line[30]
       });
-      // console.log(records.length)
-      // if(line[121] == "bonniepalmer@msn.com"){
-      
-      // console.log(line[121])
-  // }
       callback();
-    // });
+    });
   });
-}
-);
 fs.createReadStream(inputFile).pipe(parser);
-function search(nameKey, myArray){
+function search(nameKey, myArray, searchType){
 	var myNewArray = [];
     for (var i=1; i < myArray.length; i++) {
-        // if (myArray[i].ucDirectory_UcListing_lblOwner.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_lblTitle.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_lblAddress1.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_lblCity.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_lblStateProvince.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_lblZipPostal.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_lblFax.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_lblPhone1.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_hlEmail.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        // 	myArray[i].ucDirectory_UcListing_hlWebsite.toLowerCase().indexOf(nameKey.toLowerCase()) > -1
+        if(nameKey.length == 1 && searchType == 'people'){
+          if (myArray[i].ucdirectory_uclisting_lblowner.toLowerCase()[0].indexOf(nameKey.toLowerCase()[0]) > -1) {
+              myNewArray.push(myArray[i]);
+          }  
+        } else if(nameKey.length == 1 && searchType == 'business'){
+          if (myArray[i].ucdirectory_uclisting_lblcompanyname.toLowerCase()[0].indexOf(nameKey.toLowerCase()[0]) > -1 ) {
+              myNewArray.push(myArray[i]);
+          }  
+        } else if(nameKey.length == 1){
+          if (myArray[i].ucdirectory_uclisting_lblcompanyname.toLowerCase()[0].indexOf(nameKey.toLowerCase()[0]) > -1 
+            ||
+            myArray[i].ucdirectory_uclisting_lblowner.toLowerCase()[0].indexOf(nameKey.toLowerCase()[0]) > -1
 
-        // 	) {
-        //     myNewArray.push(myArray[i]);
-        // }
+            ) {
+              myNewArray.push(myArray[i]);
+          }  
+        } else{
+          if (myArray[i].ucdirectory_uclisting_lblcompanyname.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+            myArray[i].ucdirectory_uclisting_lblowner.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_lbltitle.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_lbladdress1.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_lblcity.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_lblstateprovince.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_lblzippostal.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_lblfax.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_lblphone1.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_hlemail.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
+          	myArray[i].ucdirectory_uclisting_hlwebsite.toLowerCase().indexOf(nameKey.toLowerCase()) > -1
 
-        if (myArray[i].ucdirectory_uclisting_lblowner.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_lbltitle.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_lbladdress1.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_lblcity.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_lblstateprovince.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_lblzippostal.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_lblfax.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_lblphone1.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_hlemail.toLowerCase().indexOf(nameKey.toLowerCase()) > -1 ||
-        	myArray[i].ucdirectory_uclisting_hlwebsite.toLowerCase().indexOf(nameKey.toLowerCase()) > -1
-
-        	) {
-            myNewArray.push(myArray[i]);
+          	) {
+              myNewArray.push(myArray[i]);
+          }
         }
     }
     return myNewArray;
@@ -108,54 +92,16 @@ function search(nameKey, myArray){
 
 app.get('/', function (req, res) {
     
-    if(req.query.q2 != undefined){
-
-	    // connectionNode.query('SELECT * from main where ucDirectory_UcListing_lblOwner like "%?%";',
-	    //     [req.param('q')], function (err, rows, fields) {
-	    //     if (err) {
-	    //         console.log(err);
-	    //         res.json('error');
-	    //     } else {
-	    //         console.log(rows);
-	    //         res.json(rows);
-	    //     }
-	    // });
-		
-		var currentQuery = 'SELECT * from main where ' + 
-		'LOWER(ucDirectory_UcListing_lblOwner) like \'%' + req.query.q2 + '%\' OR ' + 
-		'LOWER(ucDirectory_UcListing_lblTitle) like \'%' + req.query.q2 + '%\' OR ' + 
-		'LOWER(ucDirectory_UcListing_lblAddress1) like \'%' + req.query.q2 + '%\' OR ' + 
-		'LOWER(ucDirectory_UcListing_lblCity) like \'%' + req.query.q2 + '%\' OR ' + 
-		'LOWER(ucDirectory_UcListing_lblStateProvince) like \'%' + req.query.q2 + '%\' OR ' + 
-		'LOWER(ucDirectory_UcListing_lblZipPostal) like \'%' + req.query.q2 + '%\' OR ' + 
-		'LOWER(ucDirectory_UcListing_lblFax) like \'%' + req.query.q2 + '%\' OR ' + 
-
-		'LOWER(ucDirectory_UcListing_lblPhone1) like \'%' + req.query.q2 + '%\' OR ' + 
-		'LOWER(ucDirectory_UcListing_hlEmail) like \'%' + req.query.q2 + '%\' OR ' + 
-		'LOWER(ucDirectory_UcListing_hlWebsite) like \'%' + req.query.q2
-
-		+ '%\' order by ucDirectory_UcListing_lblOwner';
-
-	    var query = baseClient.query(currentQuery);
-		// res.json(currentQuery);
-		
-		// query.on('row', function(row) {
-		// 	res.json(row);
-		// });
-
-		query.on("row", function (row, result) {
-		    result.addRow(row);
-		});
-
-		query.on("end", function (result) {
-		    res.json(result.rows);
-		    // baseClient.end();
-		});
-
-
-    }else if(req.query.q != undefined){
+    if(req.query.q != undefined){
     	// res.send(req.query.q);	
-    	res.json(search(req.query.q, records));
+      console.log("Searching: " + req.query.q)
+      if(req.query.searchType != undefined && req.query.q.length == 1){
+        console.log('singleSearch')
+        res.json(search(req.query.q, records, req.query.searchType));
+      }else{
+        console.log('comboSearch')
+      	res.json(search(req.query.q, records));
+      }
     }else {
     	res.send('No Support yet');	
     }
